ca507142e376d3c9d6a6be505ac4424c7910d9c0
Mijung <mijung@Mijungs-MacBook-Air.local>
2018-10-09 00:33:02 -0700

    rename to tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 import * as data from './devMilestones.json';
 
